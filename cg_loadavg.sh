74fda7fcfd737871dddf5081a0fb29dc9357fae9
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2020-05-19 20:53:09 +0200

    uses python now

--- cg_loadavg.sh
+++ cg_loadavg.sh
@@ -27,22 +27,27 @@ HZ=$(getconf CLK_TCK)
 LOAD_FREQ=$((5*${HZ}))
 EXP_1=1884
 difference=$(($FIXED_1 - $EXP_1))
+E=2.718281828459045 
 
 calc_load() {
     load=$1
     running_processes=$2
-    load=$(echo "$load*$EXP_1+$running_processes*$difference" | bc)
+    # load=$(echo "$load*$EXP_1+$running_processes*$difference" | bc)
+    # load=$(echo "$load / $E + $running_processes * (1 - 1/$E)" | bc)
+    load="$(python -c "from math import exp; print($load / exp(1) + $running_processes * (1 - 1/exp(1)))")"
+    echo "l = $load"
     # load=$(($load >> $FSHIFT))
     return ${load}
 }
 
-load=0
+load="0"
 while true; do
     get_number_r_and_d_processes ${CGROUP_PATH}
     running_processes=$?
-    calc_load ${load} ${running_processes}
-    load=$?
+    # calc_load ${load} ${running_processes}
+    # load=$?
+    load="$(python -c "from math import exp; print($load / exp(1) + $running_processes * (1 - 1/exp(1)))")"
     # echo "${load}"
     echo "${load},$(cat /proc/loadavg | cut -f1 -d ' ')"
-    sleep 5
+    sleep 1
 done
